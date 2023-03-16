02e87d37417eca6cfc179d4158b81510d4ff66d7
rahgadda <gaddam.rahul@gmail.com>
2023-03-16 15:33:55 +0000

    Initial Draft

--- 02-RestAPI/sample.py
+++
@@ -1,8 +0,0 @@
-import requests 
-
-API_KEY = "ZD6K10CXBWLX5SHOE51WCUU8FC2U82N08NT" 
-message="write%20camel%20dsl%20in%20yaml%20to%20read%20csv%20file"
-
-url = "https://api.betterapi.net/youdotcom/chat?message="+message+"&key=" + API_KEY 
-json = requests.get(url).json()
-print(json["message"]) 
--- 02-RestAPI/youchat-camel/index.js
+++ 02-RestAPI/youchat-camel/index.js
@@ -1,7 +1,26 @@
+// Variables
 const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+const updateDotenv = require('update-dotenv')
 require('dotenv').config();
 
 // Load Environment Variables
 let URL = process.env.URL;
 let API_KEY = process.env.API_KEY;
 
+// Get you.com/chat
+console.log("Started Application Server");
+
+// POST WS to updated API Key
+var app = express();
+app.use(bodyParser.json());
+app.post('/apiKey', function(request, response){   
+    updateDotenv({
+        API_KEY: request.body.apiKey,
+        URL: 'https://api.betterapi.net/youdotcom/chat'
+    }).then((newEnv) => console.log('Updated API Key - ', newEnv.API_KEY))
+    
+    let API_KEY = process.env.API_KEY;
+});  
+app.listen(3000);
