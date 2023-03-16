38c799778342138e80db241feec00895b167fd73
rahgadda <gaddam.rahul@gmail.com>
2023-03-16 17:48:11 +0000

    Initial Draft

--- 01-Install/install.sh
+++ 01-Install/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Generate API https://api.betterapi.net/about/
+
 npm install youchat-camel
+npm start
+
+cd camel-youchat-generator/02-RestAPI/youchat-camel
+docker build -t rahgadda/youchat-camel .
+docker run rahgadda/youchat-camel
+docker push rahgadda/youchat-camel
--- 02-RestAPI/youchat-camel/index.js
+++ 02-RestAPI/youchat-camel/index.js
@@ -3,6 +3,9 @@ const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
 const updateDotenv = require('update-dotenv');
+const regex = /`{3}([\s\S]+?)`{3}/;
+
+// Initialize variables
 var app = express();
 app.use(bodyParser.json());
 require('dotenv').config();
@@ -10,6 +13,7 @@ require('dotenv').config();
 // Load Environment Variables
 let URL = process.env.URL;
 let API_KEY = process.env.API_KEY;
+let txt = "write camel dsl in yaml to ";
 
 // Get you.com/chat
 console.log("Started Application Server");
@@ -18,15 +22,33 @@ console.log("Started Application Server");
 app.post('/apiKey', function(request, response){   
     updateDotenv({
         API_KEY: request.body.apiKey,
-        URL: 'https://api.betterapi.net/youdotcom/chat'
+        URL: 'https://api.betterapi.net/youdotcom/chat?message='
     }).then((newEnv) => console.log('Updated API Key - ', newEnv.API_KEY))
     
-    let API_KEY = process.env.API_KEY;
+    API_KEY = process.env.API_KEY;
 })
 // POST WS to get yaml data
 .post('/camel-dsl', function(request, response){ 
+    txt = "write camel dsl in yaml to "+request.body.text;
+    
     console.log('Input Text -> '+request.body.text);
-    console.log('Updated Input Text -> write camel dsl in yaml to '+request.body.text);
+    console.log('Updated Input Text -> '+txt);
+    txt=encodeURIComponent(txt);
+    console.log('Encoded Input Text -> '+txt);
+
+    axios.get(URL+txt+"&key="+API_KEY)
+         .then(resp => {
+            let str = resp.data.message;
+            // let m;
+            // if ((m = regex.exec(str)) !== null) {
+            //     console.log(m[0]);
+            //     // m.forEach((match, groupIndex) => {
+            //     //     console.log(`Found match, group ${groupIndex}: ${match}`);
+            //     // });
+            // }
+            console.log('Chatbot Message -> '+str)
+            console.log('Extracted Message -> '+ regex.exec(str)[1]);
+          });
 });
 
 app.listen(3000);
